079b1d999d292d9e5a43bedf80aaa1dd44b9f365
Justin Hill <justin@so.dang.cool>
2025-09-20 09:03:53 -0500

    more stuff

--- yoto-playlister.mjs
+++ yoto-playlister.mjs
@@ -1,4 +1,8 @@
 const here = window.location.origin + window.location.pathname;
+const hereParams = new URLSearchParams(window.location.search);
+const clientId = "YtRJRPGAptLz7BuZWPY8ihNsOKAfDEve";
+const code = hereParams.get('code');
+const state = JSON.parse(atob(hereParams.get('state') || '') || '{}');
 
 const loginUrl = new URL("https://login.yotoplay.com/authorize");
 const addParams = (url, params) => Object.entries(params).forEach(([key, param]) => url.searchParams.append(key, param));
@@ -6,7 +10,7 @@ addParams(loginUrl, {
   audience: "https://api.yotoplay.com",
   scope: "openid",
   response_type: "code",
-  client_id: "YtRJRPGAptLz7BuZWPY8ihNsOKAfDEve",
+  client_id: clientId,
   redirect_uri: here,
   state: "def"
 });
@@ -14,8 +18,12 @@ addParams(loginUrl, {
 /* INIT */
 
 const main = document.querySelector('main');
+
 const auth = document.createElement('a');
 auth.innerText = "Authorize";
 auth.href = loginUrl.href;
 
-main.appendChild(auth);
+const debug = document.createElement('pre');
+debug.innerText = JSON.stringify({ code, state }, null, 2);
+
+[auth, debug].forEach(main.appendChild);
