d502ee42f85161ed11bdfb9f102fbba277d8da4d
Justin Hill <justin@so.dang.cool>
2025-09-24 02:29:29 -0700

    fix auth

--- page/yoto-playlister.mjs
+++ page/yoto-playlister.mjs
@@ -20,7 +20,7 @@ addParams(loginUrl, {
 });
 
 if (code) {
-  const response = await fetch('https://api.yoto-playlister.so.dang.cool/prod/playlists', { Authorization: `Bearer ${code}` });
+  const response = await fetch('https://api.yoto-playlister.so.dang.cool/prod/playlists', { headers: { Authorization: `Bearer ${code}` } });
   const body = await response.json();
   console.log({ response, body });
 }
